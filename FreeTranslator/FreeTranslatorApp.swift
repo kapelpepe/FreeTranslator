6299a6eeae1ca39c6c59239b6ffa17b36011753a
Kacper <kacper.gwiazda7@gmail.com>
2024-06-03 11:42:03 +0200

    Initial Commit

---
+++ FreeTranslator/FreeTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeTranslatorApp.swift
+//  FreeTranslator
+//
+//  Created by Kacper Gwiazda on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FreeTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
